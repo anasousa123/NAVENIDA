3982f93a1315e64fe8a8ca3e5874cef250eaba4d
anasousa123 <ana.7410062@aluno.mg.gov.br>
2026-06-19 10:36:03 +0000

    ÇÇ

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("Site carregado com sucesso!");
+console.log("NAVENIDA carregado com sucesso!");
+
+const cards = document.querySelectorAll(".card");
+
+cards.forEach(card => {
+
+card.addEventListener("mouseenter", () => {
+card.style.transform = "scale(1.05)";
+card.style.transition = "0.3s";
+});
+
+card.addEventListener("mouseleave", () => {
+card.style.transform = "scale(1)";
+});
+
+});
