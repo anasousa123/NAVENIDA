0d8ce144d4d41e427fe714598515a8702cd1de78
anasousa123 <ana.7410062@aluno.mg.gov.br>
2026-06-25 11:27:10 +0000

    lçpl

--- script.js
+++ script.js
@@ -1,1 +1,51 @@
+function abrirModal(nome, foto, descricao, preco){
 
+    document.getElementById('modal').style.display = 'block';
+
+    document.getElementById('modal-img').src = foto;
+
+    document.getElementById('modal-titulo').innerHTML = nome;
+
+    document.getElementById('modal-desc').innerHTML = descricao;
+
+    document.getElementById('modal-preco').innerHTML = preco;
+
+}
+
+function fecharModal(){
+
+    document.getElementById('modal').style.display = 'none';
+
+}
+
+window.onclick = function(event){
+
+    let modal = document.getElementById('modal');
+
+    if(event.target == modal){
+
+        modal.style.display = 'none';
+
+    }
+
+};
+
+document.querySelectorAll('.titulo-item').forEach(botao => {
+
+    botao.addEventListener('click', () => {
+
+        let conteudo = botao.nextElementSibling;
+
+        if(conteudo.style.display === 'block'){
+
+            conteudo.style.display = 'none';
+
+        } else {
+
+            conteudo.style.display = 'block';
+
+        }
+
+    });
+
+});
