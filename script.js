125c183efa9bda8be5ca7a194568b85d67200e46
anasousa123 <ana.7410062@aluno.mg.gov.br>
2026-07-03 10:43:49 +0000

    kjhgtuy

--- script.js
+++ script.js
@@ -64,3 +64,17 @@ function abrirItem(card){
     }
 
 }
+
+document.querySelectorAll(".titulo-item").forEach(botao => {
+    botao.addEventListener("click", () => {
+
+        const conteudo = botao.nextElementSibling;
+
+        if (conteudo.style.display === "block") {
+            conteudo.style.display = "none";
+        } else {
+            conteudo.style.display = "block";
+        }
+
+    });
+});
